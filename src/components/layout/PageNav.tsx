558caf8dc7dbed066f44e0126d2e46d4ddc42fee
Jose L Garcia <xpertasocial@gmail.com>
2025-12-16 06:28:35 -0600

    Redesign navigation header and floating menu

--- src/components/layout/PageNav.tsx
+++ src/components/layout/PageNav.tsx
@@ -1,11 +1,15 @@
+import FloatingMenu from "./FloatingMenu";
 import Navbar from "./Navbar";
 
 export default function PageNav() {
   return (
-    <div className="w-full border-b border-white/10 bg-carbon/90 backdrop-blur-xl">
-      <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8">
-        <Navbar />
-      </div>
-    </div>
+    <>
+      <header className="sticky top-0 z-40 w-full border-b border-white/10 bg-carbon/90 backdrop-blur-xl">
+        <div className="mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8">
+          <Navbar />
+        </div>
+      </header>
+      <FloatingMenu />
+    </>
   );
 }
